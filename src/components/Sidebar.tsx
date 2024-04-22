6c03bf72e35a75f7195d96c6ed60021f96c58b30
Tamicktom <rickpokemon38@gmail.com>
2024-04-22 19:21:16 -0300

    component sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,7 @@
+//* Libraries imports
+import { PencilLine } from "@phosphor-icons/react";
+
+//* Local imports
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -5,16 +9,20 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         src="https://www.rocketseat.com.br/eventos/rsxp/_next/image?url=%2Feventos%2Frsxp%2Fassets%2Fimages%2Fspeakers%2Fdiego-fernandes.png&w=640&q=75"
+        alt=""
         className={styles.cover}
       />
 
       <div className={styles.profile}>
+        <img src="https://github.com/tamicktom.png" alt="" className={styles.avatar} />
+
         <strong>Diego Fernandes</strong>
         <span>CTO at Rocketseat</span>
       </div>
 
       <footer>
-        <a href="">
+        <a href="/">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
